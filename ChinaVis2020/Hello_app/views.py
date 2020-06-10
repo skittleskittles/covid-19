9a2bd1b7adfc7ec6a6ed9cf0bae5f0f733de1844
“fusy0714 <fusy0714@outlook.com>
2020-06-10 10:05:31 +0800

    在django中添加前端框架

--- ChinaVis2020/Hello_app/views.py
+++ ChinaVis2020/Hello_app/views.py
@@ -4,4 +4,16 @@ from Hello_app.models import Confirmed
 
 def get_list(request):
     confirmed_data = Confirmed.objects.all()
+    return render(request, 'test.html', {'Confirmed': confirmed_data})
+
+def home(request):
+    confirmed_data = Confirmed.objects.all()
     return render(request, 'index.html', {'Confirmed': confirmed_data})
+
+def timezone(request):
+    confirmed_data = Confirmed.objects.all()
+    return render(request, 'timezone.html', {'Confirmed': confirmed_data})
+
+def yuqing(request):
+    confirmed_data = Confirmed.objects.all()
+    return render(request, 'yuqing.html', {'Confirmed': confirmed_data})
